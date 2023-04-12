bb0ce2117b5dcbf39fbc96afadf77d94a4f47992
Valerio Vacca <valerio.vacca@aulab.it>
2023-04-12 12:49:46 +0200

    chi siamo finita

--- chis-siamo.js
+++ chis-siamo.js
@@ -71,3 +71,119 @@ window.addEventListener('scroll', ()=>{
 
 
 })
+
+
+// logica circle
+
+// cattura opener
+let opener = document.querySelector('.opener');
+
+// cattura div moved
+let movedDivs = document.querySelectorAll('.moved');
+
+// variabile d'appoggio per far tornare indietro i moved
+
+let conferma = false;
+
+// array di oggetti, docenti
+
+let teachers = [
+
+
+    { name : 'Valerio', languages : ['HTML', 'CSS', 'JS', 'Pokémon'], url : './media/valerio_vacca.png'},
+    { name : 'Francesco', languages : ['HTML', 'CSS', 'Bootstrap', 'DOM'], url : './media/francesco_talamona.png'},
+    { name : 'Paola', languages : ['PHP', 'OOP', 'Laravel', 'Galga'], url : './media/paola_pichierri.png'},
+    { name : 'Robbolo', languages : ['React', 'Database', 'Manga'], url : './media/roberto_russo.png'},
+
+
+]
+
+// cattura cardWrapper
+let cardWrapper = document.querySelector('#cardWrapper');
+
+
+movedDivs.forEach((moved, i)=>{
+
+
+    moved.style.backgroundImage = `url('${teachers[i].url}')`;
+
+    // evento click per far apparire gli amici docenti
+
+    moved.addEventListener('click', ()=>{
+
+
+        // console.log(teachers[i]);
+
+        cardWrapper.innerHTML = '';
+
+        let div = document.createElement('div');
+        
+        div.classList.add('teacher-card');
+
+        div.innerHTML = `
+
+                    <p class="h3">${teachers[i].name}</p>
+                    <p>${teachers[i].languages}</p>
+
+        `;
+
+        cardWrapper.appendChild(div);
+
+
+        // catturo la singola card per cambiare immagine
+
+        let card = document.querySelector('.teacher-card');
+        
+        card.style.backgroundImage=`url('${teachers[i].url}')`;
+
+
+    })
+
+})
+
+
+// evento su opener
+
+opener.addEventListener('click', ()=>{
+
+
+    if(conferma == false){
+
+
+        conferma = true;
+
+        movedDivs.forEach( (moved, i)=>{
+
+            let angle = (360 * i) / movedDivs.length;
+    
+            moved.style.transform = `rotate(${angle}deg) translate(200px) rotate(-${angle}deg)`;
+
+            opener.innerHTML = `<i class="fa-solid fa-minus text-blackC fa-5x"></i>`;
+    
+    
+        })
+
+    } else {
+
+        conferma = false;
+
+        cardWrapper.innerHTML = '';
+
+        movedDivs.forEach( (moved)=>{
+
+            // let angle = (360 * i) / movedDivs.length;
+    
+            moved.style.transform = `rotate(0deg) translate(0px)`;
+
+            opener.innerHTML = `<i class="fa-solid fa-plus text-blackC fa-5x"></i>`;
+    
+    
+        })
+
+
+    }
+
+  
+
+
+})
